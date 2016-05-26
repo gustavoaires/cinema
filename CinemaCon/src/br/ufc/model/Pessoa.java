377d501b5ae436754df9ce970eb9b8dcd95312a6
eduardoslopes <eduardo.lopes.es@gmail.com>
2016-05-26 16:36:30 -0300

    Classe de testes do DAO

--- CinemaCon/src/br/ufc/model/Pessoa.java
+++ CinemaCon/src/br/ufc/model/Pessoa.java
@@ -63,4 +63,42 @@ public class Pessoa {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((sobrenome == null) ? 0 : sobrenome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (sobrenome == null) {
+			if (other.sobrenome != null)
+				return false;
+		} else if (!sobrenome.equals(other.sobrenome))
+			return false;
+		return true;
+	}
+	
+	
+	
 }
--- CinemaCon/src/br/ufc/testejunit/Testes_DAO.java
+++ CinemaCon/src/br/ufc/testejunit/Testes_DAO.java
@@ -3,8 +3,10 @@ package br.ufc.testejunit;
 import java.util.Calendar;
 import java.util.List;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
@@ -20,21 +22,22 @@ import junit.framework.Assert;
 @ContextConfiguration(locations = {"file:WebContent/WEB-INF/spring-context.xml"})
 @Transactional
 @TransactionConfiguration(defaultRollback=false)
-public class TesteJunit extends AbstractTransactionalJUnit4SpringContextTests {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class Testes_DAO extends AbstractTransactionalJUnit4SpringContextTests {
 	
 	@Autowired
 	private IPessoaDAO pdao;
-	
+	private static long id ;
 
 	@Test
-	public void testeRecuperar(){
-		Pessoa p = pdao.recuperar(1L);		
+	public void ctesteRecuperar(){
+		Pessoa p = pdao.recuperar(id);		
 		Assert.assertEquals("Eduardo", p.getNome());
 		
 	}
 	
 	@Test
-	public void testeInserir(){
+	public void atesteInserir(){
 		Pessoa p = new Pessoa();
 		p.setNome("Eduardo");
 		p.setSobrenome("Lopes");
@@ -42,20 +45,35 @@ public class TesteJunit extends AbstractTransactionalJUnit4SpringContextTests {
 		p.setDescricao("Forte alto bonito e sensual");
 		p.setDataNascimento(Calendar.getInstance().getTime());
 		pdao.inserir(p);
-		
 	}
 	
 	@Test
-	public void testelistar(){
+	public void btestelistar(){
 		List<Pessoa> ps = pdao.listar();
+		id = ps.get(ps.size()-1).getId();
 		Assert.assertNotNull(ps);
 	}
 	
 	@Test
-	public void testeApagar(){
-		pdao.apagar(3L);
-		Pessoa pessoa = pdao.recuperar(3L);
+	public void etesteApagar(){
+		pdao.apagar(id);
+		Pessoa pessoa = pdao.recuperar(id); 
 		Assert.assertNull(pessoa);
+	}
+	
+	@Test
+	public void dtesteAlterar(){
+		Pessoa p = new Pessoa();
+		p.setId(id);
+		p.setNome("Gustavo");
+		p.setSobrenome("Aires");
+		p.setLocalNascimento("Senador");
+		p.setDescricao("Alto magrelo feio e bobo");
+		p.setDataNascimento(Calendar.getInstance().getTime());
+		pdao.alterar(p);
+		Pessoa p2 = pdao.recuperar(p.getId());
+		Assert.assertEquals(p2, p);
+		
 	}
 	
 }
