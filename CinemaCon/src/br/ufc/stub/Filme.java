4c82832cab8d17b8e3038d4e3833b7a2259ef15f
gustavoaires <gustavo@debian>
2016-05-26 20:33:53 -0300

    Finalizando visualizar pessoa. Configurando home do projeto. Completando stub do filme.

--- CinemaCon/src/br/ufc/stub/Filme.java
+++ CinemaCon/src/br/ufc/stub/Filme.java
@@ -23,18 +23,48 @@ public class Filme {
 	@ManyToMany(mappedBy="filmes", fetch=FetchType.LAZY)
 	private List<Pessoa> pessoas;
 	
+	private String genero;
+	private Double duracao;
+	private Integer classificacao;
+	private String distribuidora;
+	private String idioma;
 	private String nome;
+	private String sinopse;
 	
+	public String getSinopse() {
+		return sinopse;
+	}
+	public void setSinopse(String sinopse) {}
+	public List<Pessoa> getPessoas() {
+		return null;
+	}
+	public void setPessoas(List<Pessoa> pessoas) {}
+	public String getGenero() {
+		return "";
+	}
+	public void setGenero(String genero) {}
+	public Double getDuracao() {
+		return null;
+	}
+	public void setDuracao(Double duracao) {}
+	public Integer getClassificacao() {
+		return null;
+	}
+	public void setClassificacao(Integer classificacao) {}
+	public String getDistribuidora() {
+		return null;
+	}
+	public void setDistribuidora(String distribuidora) {}
+	public String getIdioma() {
+		return null;
+	}
+	public void setIdioma(String idioma) {}
 	public Long getId() {
-		return id;
+		return null;
 	}
 	public String getNome() {
-		return nome;
+		return null;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+	public void setNome(String nome) {}
+	public void setId(Long id) {}
 }
